4beb6dfc26abf8edff9b6cda41971dd71592051b
alanacalbente <alanacalbente@outlook.com>
2023-04-18 23:20:16 -0300

    add noticia no Navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import "./App.css";
-import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/navbar/Navbar"
 import { Outlet } from "react-router-dom";
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <Navbar />
-      <Outlet />
-    </div>
-  );
-}
+ 
+    <div>
+      < Navbar/>
+   < Outlet/>
 
-export default App;
+    </div>
+   
+  );
+
+}
+export default App; 
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,6 +3,7 @@ import logo from "../../assets/navbar/logo.png";
 import home from "../../assets/navbar/home.png";
 import contact from "../../assets/navbar/contact.png";
 import service from "../../assets/navbar/service.png";
+import noticia from "../../assets/navbar/noticia.png";
 import { Link } from "react-router-dom";
 
 function Navbar() {
@@ -25,6 +26,9 @@ function Navbar() {
           <Link to="/contact">
             <img src={contact} alt="Contact" />
           </Link>
+          <Link to="/noticia"> 
+          <img src={noticia} alt="Noticia" />
+        </Link>
         </div>
       </div>
     </div>

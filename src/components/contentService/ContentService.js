bb1ace1b1ac7a12f92ac85c0f9567921e282e812
lfrodrigues22 <lf_rodrigues@id.uff.br>
2023-04-18 21:56:36 -0300

    add ajuste contentService

--- src/components/contentService/ContentService.js
+++ src/components/contentService/ContentService.js
@@ -1,10 +1,27 @@
+import "./contentService.css";
+import leao from "../../assets/service/leao.png";
+
 function ContentService() {
-    return (
-      <div>
-        <h1>página de contatos</h1>
-        <p> Works</p>
+  return (
+    <div className="container">
+      <div className="image">
+        <img src={leao} alt="Leao"/>
       </div>
-    );
-  }
-  
-  export default ContentService;
+
+      <div>
+        <p className="mainText">
+          {" "}
+          o Imposto de Renda é uma obrigação tributária anual que incide sobre
+          os rendimentos das pessoas físicas e jurídicas para tornar esse
+          processo mais simples e tranquilo, nossa equipe de especialistas em
+          contabilidade está preparada para cuidar de todas as etapas do seu
+          Imposto de Renda, garantindo conformidade com o a legislação vigente,
+          deduções e benefícios fiscais. Não deixe para a ultima hora, agende
+          sua consulta conosco e fique em dia com suas obrigações fiscais
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ContentService;
